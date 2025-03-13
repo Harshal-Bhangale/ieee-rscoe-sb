2ac6911b73b357e9fddbae025b7d59813fe5fb68
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-03-13 11:32:56 +0530

    Update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="w-full overflow-x-hidden">
+      <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
